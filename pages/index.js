0d0e4105bdf2c7db654823aad4d7e7023caa97ea
jackzucker69 <140255790+jackzucker69@users.noreply.github.com>
2023-07-23 16:37:14 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,80 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import {useRouter} from "next/router"
 
 export default function Home() {
+
+const myABI = [{
+    "inputs": [],
+    "name": "publicPrice",
+    "outputs" [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "refererAddress",
+        "type": "address"
+      }
+    ],
+    "name": "affiliateMarketingMint",
+    "outputs" [],
+    "stateMutability": "payable",
+    "type": "function"
+}
+]
+
+const contractAddress = "abcd"
+async function mint() {
+  try {
+    const affiliateAddress = router?.query?.r || "0x87ECbA01720182f6Bd63b8576c4388DA8950fA90"
+    console.log(affiliateAddress)
+    const publicPrice = (await readContract({
+      address: contractAddress,
+      abi: myABI,
+      functionName: "publicPrice",
+    })).toString()
+    console.log("publicPrice", publicPrice)
+    const value = ethers.BigNumber.from(tokenAmount).mul(publicPrice);
+    console.log("value", value)
+    const {hash} = await writeContract({
+      address: contractAddress,
+      abi: myABI,
+      functionName: "affiliateMarketingMint",
+      args: [
+        affiliateAddress,
+      ],
+      value: BigInt(value.toString())
+    })
+    console.log("hash", hash)
+    return null    
+  }
+  catch (error) {
+    console.error("Error in mint:", error);
+    throw (error)
+  }
+}
+
+const router = useRouter()
+const [mintState, setMintState] = useState(0)
+async function mintWrapper() {
+  try {
+    await mint()
+  } catch(err) {
+    console.log(err)
+    setMintState(-1)
+  }
+}
+
   return (
     <div className="container">
       <Head>
@@ -35,10 +107,14 @@ export default function Home() {
           </ul>
         </p>
         <p className="description">
-          Pridruzi se z mintom AnnonymousX NFTja na protokolu zkSync.
+          Pridruzi se z mintom AnnonymousX NFTja na protokolu Arbitrum.
         </p>
 
-        <button class="button-7" role="button">mint</button>
+        {
+          mintState == 0 ? <><button class="button-7" role="button">mint</button></> : <>{
+            mintState == 1 ? <>success</> : <>failed, check logs</>
+          }</>
+        }
         <p className="description">
           1. septembra povezava do zaprte skupine
         </p>
