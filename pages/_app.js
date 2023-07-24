a7b9db72cb2093faef01a5c7d90b40cda23404d4
jackzucker69 <140255790+jackzucker69@users.noreply.github.com>
2023-07-24 12:01:09 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
 import '@styles/globals.css'
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider,} from "@rainbow-me/rainbowkit";
+import { connectorsForWallets} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygonZkEvm } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+let chain = polygonZkEvm;
+
+const { chains, publicClient } = configureChains(
+  [chain],
+  [
+    //alchemyProvider({ apiKey: "" }),
+    publicProvider()
+  ]
+);
+const appName = "asdf";
+const projectId = "asdf";
+const { wallets: walletGroups } = getDefaultWallets({
+  appName: appName,
+  projectId: projectId,
+  chains,
+});
+const connectors = connectorsForWallets(walletGroups);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <WagmiConfig config={wagmiConfig}>
+    <RainbowKitProvider chains={chains}>
+      <Component {...pageProps} />
+    </RainbowKitProvider>
+    </WagmiConfig>
+  </>
 }
 
 export default Application
