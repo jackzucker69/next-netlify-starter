d3b2b8b1d0ed07d57c9945f23875a3d2fe693270
jackzucker69 <140255790+jackzucker69@users.noreply.github.com>
2023-07-24 12:08:42 +0000

    Add files via upload

---
+++ pages/help.js
@@ -0,0 +1,87 @@
+export default function Help() {
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1>Pomoč</h1>
+
+      <h2>Kontakt</h2>
+      <p>Za morebitna vprašanja in poizvedbe smo na voljo na naslednjem elektronskem naslovu: mymail@example.com</p>
+
+      <h2>O AnonymousX Slovenija</h2>
+      <p>
+        AnonymousX Slovenija je skupnost, osredotočena na decentralizirane tehnologije, svobodo govora in varnost na internetu. 
+        Združuje nas skupne vrednote globalnega gibanja Anonymous, s posebnim poudarkom na Sloveniji.
+      </p>
+
+      <h2>Kaj je DAO?</h2>
+      <p>
+        DAO (Decentralizirana avtonomna organizacija) je organizacijski model, ki temelji na tehnologiji blockchain. 
+        DAO omogoča transparentno in neodvisno samodejno organiziranje ter upravljanje skupnosti preko pametnih pogodb.
+      </p>
+
+      <h2>Kaj je NFT?</h2>
+      <p>
+        NFT (Non-Fungible Token), ali po slovensko nezamenljiv žeton, je vrsta kriptožetona, ki predstavlja edinstveno sredstvo ali predmet.
+        NFT-ji so bili uporabljeni za tokenizacijo najrazličnejših vrst sredstev, med drugim umetniških del, zbiralnih predmetov, in več.
+      </p>
+
+      <h2>Kaj je PolygonZkEVM?</h2>
+      <p>
+        PolygonZkEVM je skalabilna rešitev za Ethereum, ki uporablja tehnologijo Zero-Knowledge Proof s ciljem izboljšanja zmogljivosti transakcij ter zmanjšanja stroškov transakcij. 
+        Omogoča hitro in učinkovito izvajanje pametnih pogodb v okolju, združljivem z Ethereum Virtual Machine (EVM).
+      </p>
+
+      <h2>Kako se pridružiti in izvesti mint?</h2>
+      <p>
+        Za pridružitev AnonymousX Slovenija in izvedbo minta sledite navodilom na naši spletni strani. Izvedba minta zahteva Ether (ETH) na platformi zkEVM in MetaMask ali drugo združljivo Ethereum denarnico.
+
+        Ether lahko pridobite na različne načine, med katerimi so:
+      </p>
+
+      <ul>
+          <li>
+            <strong>Kupovanje ETH</strong>: 
+            Če še nimate ETH, ga lahko kupite na različnih platformah. Moonpay je ena izmed možnosti, kjer lahko kupite ETH (priporocamo L2 kot zksync) in ga nato prenesete na Polygon preko orbiter. Lahko pa ga kupite tudi na številnih drugih platformah, kot so MetaMask, Kraken, Binance, Coinbase in drugi.
+          </li>
+          <li>
+            <strong>OrbiterFinance</strong>: 
+            OrbiterFinance je udoben način za prenos ETH med različnimi Layer 2 omrežji, kot je zkEVM. Stroški transakcij so običajno manjši kot pri uporabi tradicionalnih mostov. Obiščite stran https://orbiter.finance/ za več informacij.
+          </li>
+          <li>
+            <strong>Uporaba uradnega mostu na platformi Polygon</strong>: 
+            To je eden izmed najpogostejših načinov. Obiščite spletno stran https://wallet.polygon.technology/, sledite navodilom in prenesite svoj ETH iz omrežja Ethereum na omrežje Polygon.
+          </li>
+        </ul>
+
+      <h2>Kako se pridružiti Discordu?</h2>
+      <p>
+        Povezava do naše skupine na Discordu bo objavljena 1. septembra. Ostanite povezani!
+      </p>
+
+      <h2>Affiliate Marketing</h2>
+      <p>
+        Naša pametne pogodbe uporabljajo affiliate marketing. TO deluje avtomatsko, vse kar rabite naredit, da dodate ?r=vaš_referral_address, in si prislužite 50% provizije od prodaje.
+        Prav tako imate možnost tiskanja QR kod, ki jih lahko delite s svojimi povezavami.
+      </p>
+
+      <h2>Odprtokodnost</h2>
+      <p>
+        Naša spletna stran je odprtokodna. Če bi želeli prispevati k njenemu razvoju, obiščite naš repozitorij na GitHubu. Tam boste našli tudi našo pametno pogodbo.
+      </p>
+
+      <h2>Uporabljene tehnologije</h2>
+      <p>
+        Naša spletna stran je bila zasnovana z uporabo tehnologije Next.js in je gostovana na platformi Netlify.
+      </p>
+
+      <h2>Bounties</h2>
+      <p>
+        Veseli bomo vsake pomoči, bodisi v obliki deljenja naše vsebine, bodisi v obliki prispevka k naši kodi. Pomoci pa bodo izplacane v obliki bountyov od nas, kasneje pa avtomatsko iz DAO. Trenutno iščemo:
+        - izboljšave naše predstavitvene strani.
+        - rešitve za IPFS za slike in metapodatke, ki bi lahko bile tudi generirane z AI,
+        - moderatorje za naš Discord strežnik, ki bi skrbeli za red in odgovarjali na vprašanja,
+        - pisce člankov,
+        - vaše lastne ideje,
+      </p>
+    </div>
+  )
+}
